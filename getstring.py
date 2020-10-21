a8d4901c6992a4c93ba883df63008102721f407c
Justin Burmesch <justin@aeon-okayama.local>
2020-10-21 15:52:34 +0900

    fixed some formatting

--- getstring.py
+++ getstring.py
@@ -1,4 +1,3 @@
-from sys import exit
 
 
 def get_string(prompt: str, max_words=0, min_words=1, max_chars=0, min_chars=1,
@@ -30,17 +29,16 @@ def get_string(prompt: str, max_words=0, min_words=1, max_chars=0, min_chars=1,
         )
 
     if max_chars > 0 and len(answer) > max_chars:
-            print(f'Error: No more than {max_chars} character(s) may be '
-                  + 'entered.')
-            return get_string(
-                prompt=prompt,
-                max_words=max_words,
-                min_words=min_words,
-                max_chars=max_chars,
-                min_chars=min_chars,
-                options=options,
-                case_sensitive_options=case_sensitive_options
-            )
+        print(f'Error: No more than {max_chars} character(s) may be entered.')
+        return get_string(
+            prompt=prompt,
+            max_words=max_words,
+            min_words=min_words,
+            max_chars=max_chars,
+            min_chars=min_chars,
+            options=options,
+            case_sensitive_options=case_sensitive_options
+        )
 
     if len(answer) < min_chars:
         print(f'Error: At least {min_chars} character(s) must be entered.')
@@ -55,32 +53,32 @@ def get_string(prompt: str, max_words=0, min_words=1, max_chars=0, min_chars=1,
         )
 
     if len(options) > 0 and case_sensitive_options and (answer not in options):
-                print(f'Error: Input options are: {options}. '
-                      + 'Options are case-sensitive.')
-                return get_string(
-                    prompt=prompt,
-                    max_words=max_words,
-                    min_words=min_words,
-                    max_chars=max_chars,
-                    min_chars=min_chars,
-                    options=options,
-                    case_sensitive_options=case_sensitive_options
-                )
-    elif len(options) > 0 and  check_non_case_options(answer, options):
-            return get_string(
-                prompt=prompt,
-                max_words=max_words,
-                min_words=min_words,
-                max_chars=max_chars,
-                min_chars=min_chars,
-                options=options,
-                case_sensitive_options=case_sensitive_options
-            )
+        print(f'Error: Input options are: {options}. '
+              + 'Options are case-sensitive.')
+        return get_string(
+            prompt=prompt,
+            max_words=max_words,
+            min_words=min_words,
+            max_chars=max_chars,
+            min_chars=min_chars,
+            options=options,
+            case_sensitive_options=case_sensitive_options
+        )
+    elif len(options) > 0 and check_non_case_options(answer, options):
+        return get_string(
+            prompt=prompt,
+            max_words=max_words,
+            min_words=min_words,
+            max_chars=max_chars,
+            min_chars=min_chars,
+            options=options,
+            case_sensitive_options=case_sensitive_options
+        )
 
     return answer
 
 
-def check_non_case_options(answer:str, options:list) -> None:
+def check_non_case_options(answer: str, options: list) -> None:
     upper_options = []
     for option in options:
         upper_options.append(option.upper())
